6d2b71d4060133a94865c9149868e17616844319
Circuandante <circuandante@gmail.com>
2019-12-04 23:18:25 -0500

    first commit DAY 1/1 #100DAYSOFCODE

--- django-redsocial/redsocial/users/models.py
+++ django-redsocial/redsocial/users/models.py
@@ -3,11 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class User(models.Model):
+class Profile(models.Model):
+    """proxy model that extends """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     interest = models.TextField(max_length=100)
     email = models.EmailField(max_length=254)
-    image = models.ImageField(height_field=300, width_field=None)
+    image = models.ImageField(upload_to='user/picture', blank=True,
+                              height_field=300, width_field=None
+                              )
     wePage = models.URLField(max_length=200)
     create = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
