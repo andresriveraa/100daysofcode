76ca91a963c6a300a4879acd201c7edd47b9f46b
Circuandante <circuandante@gmail.com>
2019-12-11 01:49:22 -0500

    Day 7/100 #100DaysOfCode: creacion de projecto en vue y login de usuario

---
+++ vue-redsocial/src/views/login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="maincontentlogin">
+    <!-- logo -->
+    <div class="logoredsocial">
+      <figure>
+        <img class="logologin" src="@/assets/logo.png" alt="">
+      </figure>
+    </div>
+    <!-- form -->
+    <div class="formDataUser">
+      <div class="contentLogin">
+        <div class="username">
+          <input v-model="user" placeholder="Username" type="text" id="username">
+        </div>
+        <div class="password">
+          <input type="text" id="password" placeholder="Contraseño">
+        </div>
+      </div>
+      <div class="buttonContent">
+        <div class="btn">Send email
+        </div>
+      </div>
+    </div>
+    <!-- endform -->
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  data() {
+    return {
+      user: '',
+    };
+  },
+  computed: {
+    ...mapState([
+      'userV',
+    ]),
+  },
+
+};
+
+</script>
+
+<style>
+@import '../assets/statics/login.css';
+</style>
